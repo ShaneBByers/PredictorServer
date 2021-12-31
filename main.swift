ff73582b56a112a72570680da62e6b4d4b68cfc0
shanebyers <shane@shane-byers.com>
2021-12-31 14:59:45 -0700

    More work towards getting web server working.

--- main.swift
+++ main.swift
@@ -7,5 +7,34 @@
 
 import Foundation
 
-print("Hello, World!")
+print("START")
 
+func getData(from url: String) -> String?
+{
+    let url = URL(string: url)
+    var returnString: String?
+    if let url = url
+    {
+        let group = DispatchGroup()
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = "SHANE".data(using: .utf8)
+        group.enter()
+        let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data
+            {
+                returnString = String(data: data, encoding: .utf8)
+            }
+            group.leave()
+        }
+        dataTask.resume()
+        group.wait()
+    }
+    return returnString
+}
+
+let returnString = getData(from: "http://www.nhl-predictor.com")
+if let printString = returnString
+{
+    print(printString)
+}
