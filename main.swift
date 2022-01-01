b8244c667da38d4b813832a06c4484b355f357f3
shanebyers <shane@shane-byers.com>
2021-12-31 18:04:26 -0700

    Working on url connections

--- main.swift
+++ main.swift
@@ -17,25 +17,53 @@ func getData(from url: String) -> String?
     {
         let group = DispatchGroup()
         var request = URLRequest(url: url)
-        request.cachePolicy = .returnCacheDataElseLoad
-        request.httpMethod = "POST"
-        request.httpBody = "SHANE".data(using: .utf8)
+//        request.httpMethod = "POST"
+//        request.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+//        request.cachePolicy = .reloadRevalidatingCacheData
+//        request.cachePolicy = .returnCacheDataElseLoad
+//        request.httpMethod = "POST"
+//        request.httpBody = "ABC".data(using: .utf8)
+//        if let _ = URLCache.shared.cachedResponse(for: request)
+//        {
+//            print("CACHED")
+//        }
+//        group.enter()
         group.enter()
-        let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data
-            {
-                returnString = String(data: data, encoding: .utf8)
-            }
+        let session = URLSession(configuration: .default)
+        let socketTask = session.webSocketTask(with: request)
+        socketTask.sendPing { error in
+            print("PONG")
             group.leave()
         }
-        dataTask.resume()
+        
+        socketTask.resume()
+//        let dataTask = session.dataTask(with: request) { data, response, error in
+//            if let data = data
+//            {
+//                returnString = String(data: data, encoding: .utf8)
+//            }
+//
+//            group.leave()
+//        }
+//
+//        dataTask.resume()
+//        request.networkServiceType = .responsiveData
+//        let cache = URLCache.shared.cachedResponse(for: request)
+//        URLCache.shared.removeAllCachedResponses()
+
         group.wait()
+
     }
+//    sleep(10000)
+//    print("DONE WITH METHOD")
     return returnString
 }
 
-let returnString = getData(from: "http://www.nhl-predictor.com/test.php")
-if let printString = returnString
+let returnString = getData(from: "http://www.nhl-predictor.com")
+if let _ = returnString
 {
-    print(printString)
+    print("DONE")
 }
+
+//let testing = getData(from: "http://www.toonhq.com")
+//print("SUPER DONE")
