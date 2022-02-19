93d7310e9d62dd44a08a45df893e6fd5e6127656
shanebyers <shane@shane-byers.com>
2022-02-19 16:42:32 -0700

    Added ConstantsTemplate.

---
+++ Constants/ConstantsTemplate.swift
@@ -0,0 +1,15 @@
+//
+//  ConstantsTemplate.swift
+//  PredictorServer
+//
+//  Created by Shane Byers on 2/19/22.
+//
+
+import Foundation
+
+enum ConstantStringsTemplate : String
+{
+    case DB_SERVER_NAME = "DB_SERVER_NAME"
+    case DB_USERNAME = "DB_USERNAME"
+    case DB_PASSWORD = "DB_PASSWORD"
+}
